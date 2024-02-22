24df407856bfc81545a8eb0f1992329280173cc8
saraaffro <saraaffro@gmail.com>
2024-02-22 13:20:43 +0100

    feat: type migration, factory, model and seeder

--- database/seeders/TypeTableSeeder.php
+++ database/seeders/TypeTableSeeder.php
@@ -4,21 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
-class DatabaseSeeder extends Seeder
+class TypeTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this -> call([
-
-            ProjectTableSeeder :: class,
-            TypeTableSeeder :: class,
-
-        ]);
+        Type :: factory() -> count(5) -> create();
     }
 }
